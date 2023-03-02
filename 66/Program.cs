e98894db9e5d01f4ca3b56bd4e839b510615a94d
IlyaShcelkanov <l0stt.ttime@gmail.com>
2023-03-02 15:11:01 +0300

    1 commit

---
+++ 66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int Sum(int M, int N)
+{
+    if (M == N) return N;
+    return Sum(M + 1, N) + M;
+}
+Console.Write("Введите M = ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(Sum(M, N));
---
+++ 68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int AkkermanFunction(int a, int b)
+{
+    if (a == 0) return b + 1;
+    if (b == 0) return AkkermanFunction(a - 1, 1);
+    return AkkermanFunction(a - 1, AkkermanFunction(a, b-1));
+}
+
+
+Console.Write("Введите M = ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите N = ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write(AkkermanFunction(M, N));
